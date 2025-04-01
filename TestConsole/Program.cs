491c9576e7db1a931587057696c9a304cb9fd8d4
Zack Atrash <111879622+ZackAlatrash@users.noreply.github.com>
2025-04-01 23:13:59 +0200

    Add files via upload

---
+++ TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+namespace TestConsole
+{
+    using Model;
+    using DAL;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuItemDAO menuItemDAO = new MenuItemDAO();
+            List<MenuItem> list = menuItemDAO.GetAllMenuItems();
+
+            foreach (MenuItem item in list)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+        }
+    }
+}
